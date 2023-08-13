06d28652bab0aa8b8d82be84af949771295f9255
hua <501014616@qq.com>
2023-08-13 15:49:42 +0800

    first commit

---
+++ src/main/java/com/hua/git/JavaClass.java
@@ -0,0 +1,12 @@
+package com.hua.git;
+
+/**
+ * @author hua
+ * @version 1.0
+ * @date 2023/8/13 15:36
+ */
+public class JavaClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
