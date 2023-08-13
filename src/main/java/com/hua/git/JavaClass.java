832e91ddcebe1fd1aac835848d691922632f427b
hua <501014616@qq.com>
2023-08-13 15:52:20 +0800

    third commit

--- src/main/java/com/hua/git/JavaClass.java
+++ src/main/java/com/hua/git/JavaClass.java
@@ -8,5 +8,8 @@ package com.hua.git;
 public class JavaClass {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 }
